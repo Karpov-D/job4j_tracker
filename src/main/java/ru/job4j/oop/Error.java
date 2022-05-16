dc00411d24a589c685ed3d647f3bce60180dfc31
Dmitry <karpov0910@mail.ru>
2022-05-16 22:45:08 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,32 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    boolean active;
+    int status;
+    String message;
+
+    public Error (boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public  Error () {
+    }
+
+    public void printInfo() {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        Error error404 = new Error(false, 404, "Not found");
+        Error error503 = new Error(false, 503, "Service Unavailable");
+        error.printInfo();
+        error404.printInfo();
+        error503.printInfo();
+    }
+}
