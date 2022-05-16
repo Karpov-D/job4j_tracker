eda43a6758ed0e3ad8ca613b8947cfbdb003405d
Dmitry <karpov0910@mail.ru>
2022-05-16 22:24:05 +0300

    1.6. Взаимодействие объектов [#166087 #301757]

--- src/main/java/ru/job4j/oop/Ball.java
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -4,8 +4,8 @@ public class Ball {
     public void tryRun(boolean condition) {
         if (condition) {
             System.out.println("Колобок съеден");
-        }
-        else
+        } else {
             System.out.println("Колобок сбежал");
+        }
     }
 }
