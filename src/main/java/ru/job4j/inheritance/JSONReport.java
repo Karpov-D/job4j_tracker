8650f358919b15a82dc585136154cced422fa212
Karpov-D <karpov0910@mail.ru>
2022-05-30 12:24:14 +0300

    4. Переопределение [#174890 #304477]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -2,11 +2,11 @@ package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
     public String generate(String name, String body) {
-        return "{\n"
+        return "{" + System.lineSeparator()
                 + "\t\"name\" : " + "\"" + name + "\""
                 + ",\n"
                 + "\t\"body\" : " + "\""
-                + body + "\""
-                + "\n}";
+                + body + "\"" + System.lineSeparator()
+                + "}";
     }
 }
