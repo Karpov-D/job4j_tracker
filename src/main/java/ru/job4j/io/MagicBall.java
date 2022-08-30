b8712e48b9ba4719e2804123714ac0f8b61e733b
Dmitry <karpov0910@mail.ru>
2022-08-30 22:33:07 +0300

    2. Scanner и чтение числа из консоли. [#179518]

--- src/main/java/ru/job4j/io/MagicBall.java
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -6,7 +6,7 @@ import java.util.Random;
 public class MagicBall {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Я великий Оракул. Что ты хочешь узнать? ");
+        System.out.println("Я великий Оракул. Что ты хочешь узнать? ");
         input.nextLine();
         int answer = new Random().nextInt(3);
         if (answer == 0) {
