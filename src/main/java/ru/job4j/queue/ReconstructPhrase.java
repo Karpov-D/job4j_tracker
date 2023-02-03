24f4a019295c9743b617fa3a18fa5d4f331cc3ca
Dmitry <karpov0910@mail.ru>
2023-02-03 06:47:49 +0300

    2. Интерфейс Deque [#504943 #387466]

--- src/main/java/ru/job4j/queue/ReconstructPhrase.java
+++ src/main/java/ru/job4j/queue/ReconstructPhrase.java
@@ -21,8 +21,7 @@ public class ReconstructPhrase {
             if (i % 2 != 0) {
                 builder.append(ch);
                 i++;
-            }
-            else {
+            } else {
                 i--;
             }
         }
